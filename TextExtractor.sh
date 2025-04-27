5a06367ec9cea67280c09e9928e2e4df5b4b0491
Vito Barra <vb.vitobarra@gmail.com>
2025-04-27 18:05:09 +0200

    [FIX] fix text extractor encoding on windows

--- TextExtractor.sh
+++ TextExtractor.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+file_name="$0"
+
 if [ "$#" -ne 2 ]; then
-    echo "Uso: ./estrai_testo.sh input.html output.txt"
+    echo "usage: $file_name input.html output.txt"
     exit 1
 fi
 
 input_file="$1"
 output_file="$2"
+
 
 # Detect the encoding
 encoding=$(file -bi "$input_file" | sed -n 's/.*charset=\(.*\)/\1/p')
@@ -16,14 +19,14 @@ encoding=$(echo "$encoding" | tr '[:upper:]' '[:lower:]')
 
 # Only convert if it's not already utf-8
 if [ "$encoding" != "utf-8" ]; then
-    echo "using temp file with UTF-8 encoding"
+    echo "Using temp file with UTF-8 encoding"
     tmp_file=$(mktemp)
     iconv -f "$encoding" -t utf-8 "$input_file" > "$tmp_file"
-    html2text "$tmp_file" > "$output_file"
+    lynx -dump -nolist "$tmp_file" > "$output_file"
     rm "$tmp_file"
 else
-    echo "converting directly"
-    html2text "$input_file" > "$output_file"
+    echo "Converting directly"
+    lynx -dump -nolist "$input_file" > "$output_file"
 fi
 
-echo "Testo estratto salvato in: $output_file"
+echo "text extracted in : $output_file"
