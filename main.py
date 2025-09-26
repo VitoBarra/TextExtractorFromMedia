b6bac2a02aa884e5a00346f02ec12fb8701918be
VitoBarra <vb.vitobarra@gmail.com>
2025-09-27 01:39:11 +0200

    [REFACTOR] refactored audio process pipeline

--- main.py
+++ main.py
@@ -1,52 +1,39 @@
-import os
-from pathlib import Path
-
 from DataProcessing import RAW_VIDEO_FOLDER, RAW_AUDIO_FOLDER, \
-    SPLITTED_VIDEO_FOLDER, HTML_OUTPUT_FOLDER, OUTPUT_TRANSCRIPT, ENHANCED_AUDIO_FOLDER
-from DataProcessing.AudioEnhancer import enhance_audio_in_chunks
-from DataProcessing.AudioToText import transcribe_audio
+    SPLITTED_AUDIO_FOLDER, SPLITTED_VIDEO_FOLDER, HTML_OUTPUT_FOLDER, OUTPUT_TRANSCRIPT, ENHANCED_AUDIO_FOLDER
+from DataProcessing.AudioEnhancer import EnhanceAudioFolder
+from DataProcessing.AudioExtractor import AudioFormat
+from DataProcessing.AudioToText import TranscribeAudioFolder
 from DataProcessing.HTMLToMDConverter import ExtractTextFromFolder
-from DataProcessing.MediaSplitter import SplitVideoInFolder
+from DataProcessing.MediaSplitter import SplitMediaInFolder
 from WebScraper.VzardAIUploader import UploadVideoFolder
-
-
 
 # --- Settings ---
 
 HEADLESS_MODE = True
 
 def ProcessVideo():
-    SplitVideoInFolder(RAW_VIDEO_FOLDER,SPLITTED_VIDEO_FOLDER, 30)
+    # AudioFolderToVideo(RAW_AUDIO_FOLDER, RAW_VIDEO_FOLDER, VideoFormat.MP4, overwrite=False)
+    SplitMediaInFolder(RAW_VIDEO_FOLDER, SPLITTED_VIDEO_FOLDER, 60 * 30)
     jobToDo = True
     while jobToDo:
         jobToDo = not UploadVideoFolder( RAW_VIDEO_FOLDER, HTML_OUTPUT_FOLDER, HEADLESS_MODE)
     ExtractTextFromFolder(HTML_OUTPUT_FOLDER,OUTPUT_TRANSCRIPT)
 
 def ProcessAudio():
-    files = [Path(f) for f in os.listdir(RAW_AUDIO_FOLDER) if os.path.isfile(RAW_AUDIO_FOLDER/ f)]
-    audioProjects = [Path(dir).name for dir in os.listdir(ENHANCED_AUDIO_FOLDER) if os.path.isdir(ENHANCED_AUDIO_FOLDER / dir)]
-    files_NotEnhanced = [f for f in files if f.stem not in audioProjects]
-
-    for f in files_NotEnhanced:
-        enhance_audio_in_chunks(
-            input_file=RAW_AUDIO_FOLDER/ f.name,
-            output_file=ENHANCED_AUDIO_FOLDER/f.stem/f"{f.stem}.wav",
-            raw_chunk_dir=ENHANCED_AUDIO_FOLDER/f.stem/"RawAudioChunk",
-            enhanced_chunk_dir=ENHANCED_AUDIO_FOLDER/f.stem/"enhancedAudioChunk",
-            chunk_duration=60*15,
-            lowcut=100,
-            highcut=6000,
-            compress_threshold_db=-30,
-            compress_ratio=4,
-            gain_db=8,
-        )
-
-    for f in files:
-        transcribe_audio(ENHANCED_AUDIO_FOLDER/f.stem/f"{f.stem}.wav", model_size="medium",
-                         save_to=OUTPUT_TRANSCRIPT/f.stem/"transcript.md")
+    # VideoFolderToAudio(RAW_VIDEO_FOLDER, RAW_AUDIO_FOLDER, AudioFormat.WAV, overwrite=False)
+    SplitMediaInFolder(RAW_AUDIO_FOLDER, SPLITTED_AUDIO_FOLDER, 60 * 15)
+    EnhanceAudioFolder(SPLITTED_AUDIO_FOLDER, ENHANCED_AUDIO_FOLDER, AudioFormat.WAV,
+        lowcut=100,
+        highcut=6000,
+        compress_threshold_db=-30,
+        compress_ratio=4,
+        gain_db=8,
+    )
+    TranscribeAudioFolder(ENHANCED_AUDIO_FOLDER, OUTPUT_TRANSCRIPT)
 
 if __name__ == '__main__':
-        # ProcessVideo()
+        ProcessVideo()
         ProcessAudio()
+
 
 
